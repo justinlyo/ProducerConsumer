e5ba3e0373dbf7487b1ff459a3b5bacc6c147201
justinlyo <122340030+justinlyo@users.noreply.github.com>
2024-04-05 04:28:20 +0000

    Open memory, allocate space, and map sharedTable in consumer

--- consumer.cpp
+++ consumer.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "sharedMemory.hpp"
 
 int main() {
     std::cout << "Consumer begins\n";
+    // Opening memory up
+    int memory = shm_open(NAME, O_CREAT | O_RDWR, 0700);
 
+    // Allocate appropriate amount of space
+    if(ftruncate(memory, SIZE)){
+        std::cout << "Consumer: ftruncate failed\n";
+        exit(-1);
+    }
+    // Map sharedTable
+    struct table* sharedTable = (struct table*)mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, memory, 0);
+    if (sharedTable == MAP_FAILED){
+        std::cout << "Consumer: Map Failed\n";
+        exit(-1);
+    }
+
+
+    // Close the shared memory object
+    munmap(sharedTable,SIZE);
+    close(memory);
+    shm_unlink(NAME);
+    std::cout << "Consumer ends\n";
     return 0;
 }
