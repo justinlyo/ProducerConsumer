01e05342ade707b37bf4ffe4fe9a6208083de4da
justinlyo <122340030+justinlyo@users.noreply.github.com>
2024-04-05 04:14:38 +0000

    Add main functions to producer and consumer

---
+++ consumer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sharedMemory.hpp"
+
+int main() {
+    std::cout << "Consumer begins\n";
+
+    return 0;
+}
---
+++ producer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sharedMemory.hpp"
+
+int main() {
+    std::cout << "Producer begins\n";
+
+    return 0;
+}
