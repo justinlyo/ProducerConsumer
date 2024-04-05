80de36ef1c8a58d2928f4f023878ebe9c5711ae2
justinlyo <122340030+justinlyo@users.noreply.github.com>
2024-04-05 04:33:44 +0000

    Open, close, and unlink semaphores for producer

--- producer.cpp
+++ producer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <semaphore.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -23,6 +24,21 @@ int main() {
 
     sharedTable->in = 0;
     sharedTable->out = 0;
+
+    // Open semaphores
+    sem_t *full = sem_open("full", O_CREAT, 0700, 0); // Semaphore for when table is full (+ helps with entering critical section)
+    sem_t *empty = sem_open("empty", O_CREAT, 0700, bufferSize); // Semaphore for when table is empty (+ helps with entering crtiical section)
+    sem_t *mutex = sem_open("mutex", O_CREAT, 0700, 1); // Semaphore for critical section
+
+    // Close semaphores
+    sem_close(full);
+    sem_close(empty);
+    sem_close(mutex);
+
+    // Unlink semaphores
+    sem_unlink("full");
+    sem_unlink("empty");
+    sem_unlink("mutex");
 
     // Close the shared memory object
     munmap(sharedTable,SIZE);
