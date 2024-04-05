9691b49ee43b4ee908b9f95c5a4d6faaf02701d9
justinlyo <122340030+justinlyo@users.noreply.github.com>
2024-04-05 04:18:49 +0000

    Add necessary data in sharedMemory

---
+++ sharedMemory.hpp
@@ -0,0 +1,13 @@
+
+
+
+struct table {
+    int data[2];
+    int in;
+    int out;
+};
+
+
+static const int SIZE = sizeof(table);
+static const int bufferSize = 2;
+static const char* NAME = "/producer_consumer_jlyogky";
