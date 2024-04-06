97d34d71a85c8540caa555cec69cc4dd5916a871
justinlyo <122340030+justinlyo@users.noreply.github.com>
2024-04-05 22:33:49 -0400

    Update sharedMemory.hpp

--- sharedMemory.hpp
+++ sharedMemory.hpp
@@ -1,5 +1,4 @@
-
-
+// Shared Memory
 
 struct table {
     int data[2];
@@ -7,9 +6,7 @@ struct table {
     int out;
 };
 
-
 static const int SIZE = sizeof(table);
 static const int bufferSize = 2;
-static const char* NAME = "/producer_consumer_jlyogky";
-
-static const int totalProduction = 15;
+static const char* NAME = "/producer_consumer_jlyogky"; // Name of shared memory
+static const int totalProduction = 15; // Total amount that will be produced
